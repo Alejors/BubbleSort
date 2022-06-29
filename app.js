d7879bda9ac45a0efef1751843e1b19df35ea742
alejo <alejoatria@gmail.com>
2022-06-29 12:15:14 -0400

    Se replantea formulación de mazo hacia generar un array. Se termina función sort con log por cada vuelta. Se agrega títulos de original y sort log.

--- app.js
+++ app.js
@@ -1,164 +1,162 @@
-var contador = 0;
+let deck = [];
 
 window.onload = function () {
   console.log('Sorting stuff out');
+
   //BOTON GENERAR CARTAS
   let boton = document.getElementById("draw");
   let amount = document.getElementById("cards");
   boton.addEventListener('click', (e) => {
     otracarta(amount.value);
     amount.value = "";
+    let cuerpo = document.querySelector('.row');
+    let deckParaHtml = []
+    for (let i = 0; i < deck.length; i++) {
+      deckParaHtml.push(deck[i].join(''));
+    }
+    cuerpo.innerHTML = deckParaHtml.join('');
+    console.log(deck);
   });
+
+  //BOTON QUE ORDENA CARTAS
   let sorter = document.getElementById("sort");
   sorter.addEventListener('click', (e) => {
-    bubbleSort();
+    bubbleSort(deck);
   });
 };
 
 //FUNCION QUE ENTREGA NUMERO
 function getnumber() {
+  let char = '';
+
   let number = Math.floor(Math.random() * 13 + 1);
   if (number == 13) {
-    let K = document.createTextNode('K');
-    return K;
+    char += 'K';
   }
   else if (number == 12) {
-    let Q = document.createTextNode('Q');
-    return Q;
+    char += 'Q';
   }
   else if (number == 11) {
-    let J = document.createTextNode('J');
-    return J;
+    char += 'J';
   }
   else if (number == 1) {
-    let A = document.createTextNode('A');
-    return A;
+    char += 'A';
   }
   else {
-    let text = document.createTextNode(`${number}`);
-    return text;
+    char += `${number}`;
+  }
+  return char;
+}
+
+function getsuit() {
+  let selectorpinta = Math.floor(Math.random() * 4);
+  if (selectorpinta == 0) {
+    return '<span style="color: red;">♥</span>';
+  }
+
+  else if (selectorpinta == 1) {
+    return '<span style="color: red;">♦</span>';
+  }
+
+  else if (selectorpinta == 2) {
+    return '<span>♠</span>';
+  }
+
+  else {
+    return '<span>♣</span>';
   }
 }
+
+function getestilo(pinta){
+  
+  let char = '<span';
+
+  if (pinta.includes('red')) {
+    char += ' style="color: red;">';
+  }
+  else {
+    char += '>';
+  }
+  return char;
+}
+
 //FUNCION QUE CREA CARTA
 function otracarta(num) {
 
-  let cuerpo = document.querySelector('.row');
-
   for (let i = 0; i < num; i++) {
 
-    let nuevacarta = document.createElement("div");
-    nuevacarta.classList.add("carta", "col-3");
-    let top = document.createElement("div");
-    top.classList.add(`suit${contador}`);
-    top.setAttribute("id", "top");
-    nuevacarta.appendChild(top);
-    let mid = document.createElement("div");
-    mid.setAttribute("id", `center${contador}`);
-    nuevacarta.appendChild(mid);
-    let btm = document.createElement("div");
-    btm.classList.add(`suit${contador}`);
-    btm.setAttribute("id", "bottom");
-    nuevacarta.appendChild(btm);
-    cuerpo.appendChild(nuevacarta);
-
-    //DAR NUMERO A LA CARTA NUEVA
-    var valor1 = document.getElementById(`center${contador}`);
-    valor1.appendChild(getnumber());
-
-    //DAR PINTA A LA CARTA NUEVA
-    var pinta = document.querySelectorAll(`.suit${contador}`);
-    let selectorpinta = Math.floor(Math.random() * 4);
-    if (selectorpinta == 0 || selectorpinta == 1) {
-      document.querySelector(`#center${contador}`).style.color = 'red';
-      if (selectorpinta == 0) {
-        for (let i = 0; i < pinta.length; i++) {
-          pinta[i].innerHTML = '♥';
-          pinta[i].style.color = "red";
-        }
-      }
-      else {
-        for (let i = 0; i < pinta.length; i++) {
-          pinta[i].innerHTML = '♦';
-          pinta[i].style.color = 'red';
-        }
-      }
-    }
-    else if (selectorpinta == 2) {
-      for (let i = 0; i < pinta.length; i++) {
-        pinta[i].innerHTML = '♠';
-      }
-    }
-    else {
-      for (let i = 0; i < pinta.length; i++) {
-        pinta[i].innerHTML = '♣';
-      }
-    }
-    contador++;
+    let pinta = getsuit();
+    let estilo = getestilo(pinta)
+    let valor = getnumber();
+    let carta = [`<div class="carta col-3"><div class="suit" id="top">${pinta}</div><div id="center">${estilo}`,`${valor}`,`</span></div><div class="suit" id="bottom">${pinta}</div></div>`];
+    deck.push(carta);
   }
-
 }
 
-function bubbleSort() {
-  let arr = document.querySelectorAll(".carta");
-
-  if (arr.length == 0) {
+function bubbleSort(arr) {
+  let array = [...arr];
+  if (array.length == 0) {
     window.alert("No cards to sort!");
   }
   else {
-    let wall = arr.length - 1;
+    let wall = array.length - 1;
+    let counter = 0;
 
     while (wall > 0) {
-
       for (let i = 0; i < wall; i++) {
+        
         let auxi = 0;
         let auxj = 0;
 
-        if (arr[i].childNodes[1].lastChild.nodeValue === 'K') {
+        if (array[i][1] === 'K') {
           auxi = 13;
         }
-        else if (arr[i].childNodes[1].lastChild.nodeValue === 'Q') {
+        else if (array[i][1] === 'Q') {
           auxi = 12;
         }
-        else if (arr[i].childNodes[1].lastChild.nodeValue === 'J') {
+        else if (array[i][1] === 'J') {
           auxi = 11;
         }
-        else if (arr[i].childNodes[1].lastChild.nodeValue === 'A') {
+        else if (array[i][1] === 'A') {
           auxi = 14;
         }
         else {
-          auxi = parseInt(arr[i].childNodes[1].lastChild.nodeValue);
+          auxi = parseInt(array[i][1]);
         }
 
-        if (arr[i + 1].childNodes[1].lastChild.nodeValue == 'K') {
+        if (array[i + 1][1] === 'K') {
           auxj = 13;
         }
-        else if (arr[i + 1].childNodes[1].lastChild.nodeValue == 'Q') {
+        else if (array[i + 1][1] === 'Q') {
           auxj = 12;
         }
-        else if (arr[i + 1].childNodes[1].lastChild.nodeValue == 'J') {
+        else if (array[i + 1][1] === 'J') {
           auxj = 11;
         }
-        else if (arr[i + 1].childNodes[1].lastChild.nodeValue == 'A') {
+        else if (array[i + 1][1] === 'A') {
           auxj = 14;
         }
         else {
-          auxj = parseInt(arr[i + 1].childNodes[1].lastChild.nodeValue);
+          auxj = parseInt(array[i + 1][1]);
         }
 
         if (auxi > auxj) {
-          let aux = arr[i].childNodes[1].lastChild.nodeValue;
-          arr[i].childNodes[1].lastChild.nodeValue = arr[i + 1].childNodes[1].lastChild.nodeValue;
-          arr[i + 1].childNodes[1].lastChild.nodeValue = aux;
-          let linea = document.createElement("div");
-          linea.classList.add("row");
-          let nuevoarray = [];
-          for(let k = 0; k < arr.length; k++){
-            nuevoarray.push(arr[k]);
-          }
-          console.log(nuevoarray);
-          document.querySelector(".container-flex").appendChild(linea);
+          let aux = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = aux;
         }
 
+        let linea = document.createElement("div");
+        linea.classList.add("row");
+               
+        let arrayParaHtml = [];
+        for (let i = 0; i < array.length; i++) {
+          arrayParaHtml.push(array[i].join(''));
+        }
+        arrayParaHtml.unshift(`<h3>${counter}</h3>`);
+        linea.innerHTML = arrayParaHtml.join('');
+        document.querySelector(".container-flex").appendChild(linea);
+        counter++;
       }
       wall--;
     }
